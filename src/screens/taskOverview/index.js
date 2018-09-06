9737c1cddf9d1ed4a38fbb43d2d38d5ce61cf193
Gaurav Ahuja <gauravahuja.me@gmail.com>
2018-09-06 17:33:07 +0530

    Avatar on landing screen

--- src/screens/taskOverview/index.js
+++ src/screens/taskOverview/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StatusBar, StyleSheet, FlatList } from 'react-native';
-import { Text, Icon, Button } from 'react-native-elements';
+import { Text, Icon, Button, Avatar } from 'react-native-elements';
 import LinearGradient from 'react-native-linear-gradient';
 import _ from 'lodash';
 
@@ -19,7 +19,7 @@ export default class TaskOverviewScreen extends React.Component {
         this.setActiveColor = this.setActiveColor.bind(this);
         this.viewableThreshold = { viewAreaCoveragePercentThreshold: 50 };
         this.state = {
-            firstName: 'Gaurav',
+            firstName: 'Jensen',
             activeColor: '#568cff',
             lists: lists,
         }
@@ -49,11 +49,20 @@ export default class TaskOverviewScreen extends React.Component {
                     <Icon name="keyboard-arrow-left" color="white" size={28} />
                     <Icon name="more-horiz" color="white" size={28} />
                 </View>
-                <View style={{ paddingLeft: 50, paddingVertical: 30 }}>
-                    <Text style={{ fontSize: 34, fontWeight: '600', color: 'white' }}>Hello {this.state.firstName}</Text>
-                    <Text style={{ fontSize: 14, fontWeight: '400', color: 'white' }}>
-                        Nice to see you again
-                    </Text>
+                <View style={{ paddingHorizontal: 40, paddingVertical: 30, flexDirection: 'row', alignItems: 'center' }}>
+                    <Avatar
+                        medium
+                        rounded
+                        source={{uri: "https://randomuser.me/api/portraits/men/75.jpg"}}
+                        containerStyle={{ elevation: 5 }}
+                        avatarStyle={{ borderWidth: 2, borderColor: 'white' }} />
+                    <View style={{ width: 15 }} />
+                    <View>
+                        <Text style={{ fontSize: 34, fontWeight: '600', color: 'white' }}>Hello {this.state.firstName}</Text>
+                        <Text style={{ fontSize: 14, fontWeight: '400', color: 'white' }}>
+                            Nice to see you again
+                        </Text>
+                    </View>
                 </View>
                 <View style={{ justifyContent: 'center' }}>
                     <FlatList
